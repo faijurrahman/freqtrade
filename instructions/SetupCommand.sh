af30531c6f6eadc38b7d32bafce25f077099532a
FAIJUR RAHMAN <faijur@gmail.com>
2023-01-18 12:54:21 +0000

    Reorganize the document

--- instructions/SetupCommand.sh
+++ instructions/SetupCommand.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#===================================================================================================================
+# Running Ubuntu on Windows WSL Related
+#===================================================================================================================
 # Please following this guide to prepare Ubuntu in windows WSL
 # Guide: https://learn.microsoft.com/en-us/windows/wsl/tutorials/gui-apps
 
@@ -23,6 +26,23 @@
 # Configure the package: sudo dpkg -i google-chrome-stable_current_amd64.deb
 # To launch, enter: google-chrome
 
+
+
+#===================================================================================================================
+# Git Related
+#===================================================================================================================
+git init
+git remote -v
+git remote add origin https://github.com/faijurrahman/freqtrade.git
+git config --global user.email "faijur@gmail.com"
+git config --global user.name "Faijur Rahman"
+
+
+
+
+#===================================================================================================================
+# Setup FreqTrade Related
+#===================================================================================================================
 # The installation commands are takend from here: https://youtu.be/VHvikJmQrVM
 # Install pre-requisite packages for freqtrade
 sudo apt-get update
@@ -32,20 +52,12 @@ sudo apt install -y python3-venv
 sudo apt install -y python3-dev
 sudo apt install -y python3-pandas
 
-# Configure git
-git init
-git remote -v
-git remote add origin https://github.com/faijurrahman/freqtrade.git
-git config --global user.email "faijur@gmail.com"
-git config --global user.name "Faijur Rahman"
-
 # Download stable source code from github
 # git clone https://github.com/freqtrade/freqtrade.git (Original)
 #cd freqtrade
 #git checkout stable
 git clone https://github.com/faijurrahman/freqtrade.git
 cd freqtrade
-
 
 # Setup the freqtrade binaries
 ./setup.sh -i
@@ -55,6 +67,12 @@ source ./.env/bin/activate
 freqtrade --help
 # Please check here for all commands: https://www.freqtrade.io/en/stable/utils/
 
+
+
+
+#===================================================================================================================
+# Preparing the Initial Environment of FreqTrade Related
+#===================================================================================================================
 # Create user_data & config.json and then update followings:
 # Details of config can be found here: https://www.freqtrade.io/en/stable/configuration/
 freqtrade create-userdir --userdir user_data
@@ -75,18 +93,10 @@ freqtrade download-data --config config.json --days 999 -t 5m 15m 30m 1h 2h 4h 1
 freqtrade list-data
 
 
-# Backtesting the data. Details document of Backtesting: https://www.freqtrade.io/en/stable/backtesting/
-freqtrade backtesting --config config.json --strategy SampleStrategy
-freqtrade backtesting --config config.json --strategy SampleStrategy --timerange=20210101-20211001
-freqtrade backtesting --config config.json --strategy SampleStrategy --timerange=20210101-20211001 --timeframe=4h
-freqtrade backtesting-show ??
-freqtrade backtesting-analysis ??
 
-# Backtesting with WebUI
-freqtrade webserver --config config.json
-
-
-
+#===================================================================================================================
+# Trade Command Related
+#===================================================================================================================
 # Start Trading but in dryrun only to test the WebUI
 # Details help of WebUI here: https://www.freqtrade.io/en/stable/rest-api
 freqtrade install-ui
@@ -101,3 +111,44 @@ freqtrade trade --config config.json --strategy ReinforcedSmoothScalp --strategy
 
 # Investigating old trades
 freqtrade show-trades --db-url sqlite:///tradesv3.dryrun.sqlite
+
+
+#===================================================================================================================
+# FreqAI Trade Related
+#===================================================================================================================
+# Details document here: https://www.freqtrade.io/en/stable/freqai
+
+# Running FreqAI: https://www.freqtrade.io/en/stable/freqai-running
+freqtrade trade --strategy FreqaiExampleStrategy --config config_freqai.example.json --freqaimodel LightGBMRegressor
+
+# Reinforcement Learning: https://www.freqtrade.io/en/stable/freqai-reinforcement-learning
+freqtrade trade --freqaimodel ReinforcementLearner --strategy MyRLStrategy --config config.json
+
+
+
+#===================================================================================================================
+# Backtesting Related
+#===================================================================================================================
+# Backtesting the data. Details document of Backtesting: https://www.freqtrade.io/en/stable/backtesting/
+freqtrade backtesting --config config.json --strategy SampleStrategy
+freqtrade backtesting --config config.json --strategy SampleStrategy --timerange=20210101-20211001
+freqtrade backtesting --config config.json --strategy SampleStrategy --timerange=20210101-20211001 --timeframe=4h
+freqtrade backtesting-show ??
+freqtrade backtesting-analysis ??
+
+# Backtesting with WebUI
+freqtrade webserver --config config.json
+
+
+
+#===================================================================================================================
+# Hyperparameter Optimization Related
+#===================================================================================================================
+# Details here: https:https:
+#      //www.freqtrade.io/en/stable/hyperopt 
+#      //www.freqtrade.io/en/stable/utils
+freqtrade hyperopt-list
+freqtrade hyperopt-list --profitable --no-details
+freqtrade hyperopt-show -n 168
+freqtrade hyperopt-show --best -n -1 --print-json --no-header
+
